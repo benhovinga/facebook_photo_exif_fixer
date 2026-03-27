997ca895329e08a3897e0dd0889cfad36be4ce57
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2026-03-27 13:31:08 -0300

    Doing some testing

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 import sys
 
+from bs4 import BeautifulSoup
 
+
+# Testing with args ["\\\\10.137.1.11\\home\\backups\\Facebook Archive\\posts\\album\\0.html"]
 def main(args):
-    print(args)
+    if len(args) < 1:
+        sys.exit("Missing file path as argument")
+
+    # Open the HTML file and parse
+    file_path = args[0]
+    print(f"Opening HTML file '{file_path}'")
+    with open(file_path, "r") as fs:
+        soup = BeautifulSoup(fs, "html.parser")
+
+    # Extract the page title (usually album name)
+    page_title = soup.title.string if soup.title else ""
+    print(f"Opened HTML file with title '{page_title}'")
+
+    # Get the main element containing the photos
+    main = soup.find(role="main")
+    if main == None:
+        sys.exit(
+            "Cannot locate HTML tag with attribute \"role='main'\". Unable to continue."
+        )
 
 
 if __name__ == "__main__":
